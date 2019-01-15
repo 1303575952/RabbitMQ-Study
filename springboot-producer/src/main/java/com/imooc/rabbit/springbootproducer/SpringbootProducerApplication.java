0acf7c90c243598a77eef1611ba09fe21505e190
1303575952@qq.com <1303575952@qq.com>
2019-01-15 23:16:28 +0800

    init springboot-producer

---
+++ springboot-producer/src/main/java/com/imooc/rabbit/springbootproducer/SpringbootProducerApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.rabbit.springbootproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProducerApplication.class, args);
+	}
+
+}
+
